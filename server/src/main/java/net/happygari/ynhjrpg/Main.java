9a5af0acb39a45fd7efe5581e44d1c78e361b836
Frost Kim <yojokem@naver.com>
2024-06-18 17:45:19 +0900

    대충

--- server/src/main/java/net/happygari/ynhjrpg/Main.java
+++ server/src/main/java/net/happygari/ynhjrpg/Main.java
@@ -7,10 +7,20 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-    
     @Override
-    public void onEnable(){
+    public void onLoad() {
+        System.out.println("로딩 중..");
+    }
+
+    @Override
+    public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        System.out.println("반갑노 게이야..");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("잘가라 게이야..");
     }
 
     @EventHandler
